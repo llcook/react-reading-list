ee4aa98b74b862db23f44f546f09afb0fc7d33ed
watercolorhorse <29869241+llcook@users.noreply.github.com>
2020-11-09 21:57:57 -0700

    test: h1 change

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -37,7 +37,7 @@ class Detail extends Component {
         <Row>
           <Col size="md-10 md-offset-1">
             <article>
-              <h1>Synopsis</h1>
+              <h1>What it is</h1>
               <p>{this.state.book.synopsis}</p>
             </article>
           </Col>
